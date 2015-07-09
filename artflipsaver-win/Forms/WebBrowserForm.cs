0966aa8b4d11ea6f59953b39b2fc8bd23e91d04b
Gabriel Branchaud Rubenovitch <gabriel.branchaud@gmail.com>
2015-07-08 23:04:42 -0400

    added preliminary mousemove detection (not working)

--- artflipsaver-win/Forms/WebBrowserForm.cs
+++ artflipsaver-win/Forms/WebBrowserForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -23,6 +24,7 @@ namespace ArtFlipSaver.Forms
         {
             Cursor.Hide();
             goFullScreen();
+            Application.AddMessageFilter(new DetectSignificantMouseMove());
         }
 
         private void browser_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
@@ -34,6 +36,39 @@ namespace ArtFlipSaver.Forms
         {
             this.FormBorderStyle = FormBorderStyle.None;
             this.WindowState = FormWindowState.Maximized;
+        }
+
+        [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.UnmanagedCode)]
+        private class DetectSignificantMouseMove : IMessageFilter
+        {
+            private const int MOUSE_MOVE = 0x0200;
+            private const int MOVEMENT_THRESHOLD = 10;
+            private Point previousPosition = Point.Empty;
+
+            public bool PreFilterMessage(ref Message m)
+            {
+                if (m.Msg != MOUSE_MOVE)
+                {
+                    return false;
+                }
+                Point currentMousePos = Cursor.Position;
+                if (previousPosition == Point.Empty)
+                {
+                    previousPosition = currentMousePos;
+                }
+
+                if (MouseMoveWasSignificant(previousPosition, currentMousePos))
+                {
+                    Console.WriteLine("Mouse move was significant. TODO: Fire Event to indicate time to close.");
+                }
+                return false;
+            }
+
+            private bool MouseMoveWasSignificant(Point previousPosition, Point currentMousePos)
+            {
+                return Math.Abs(previousPosition.X - currentMousePos.X) > MOVEMENT_THRESHOLD ||
+                    Math.Abs(previousPosition.Y - currentMousePos.Y) > MOVEMENT_THRESHOLD;
+            }
         }
     }
 }
